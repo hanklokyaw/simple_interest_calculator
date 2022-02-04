95992c5bb6f0277a9e716a8f248bb01b9ed4db55
hanklokyaw <hank.lo.kyaw@gmail.com>
2022-02-04 16:03:22 +0000

    new released

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    if (principal <=0){
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        }
+        else{
+        var rate = document.getElementById("rate").value;
+        var years = document.getElementById("years").value;
+        var interest = principal * years * rate / 100;
+
+        //var d = new Date();
+        var year = new Date().getFullYear() + parseInt(years);
+
+        document.getElementById("result").innerHTML = "<div>If you deposit <span class=\"decorate\">"+principal+"</span>,</br>at an interest rate of <span class=\"decorate\">"+rate+"%</span>.</br>You will receive an amount of <span class=\"decorate\">"+interest+"</span>,</br>in the year <span class=\"decorate\">"+year+"</span></div>";
+    }
+
 }
-        
+
+function rate_val(val){
+    document.getElementById('rate').value=val;
+}
